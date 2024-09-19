0cd2385960878123878a553a53ff67dc762c239b
Aidan Mahoney <21AMahoney@heckgrammar.co.uk>
2024-09-19 09:56:52 +0100

    latest work

--- ValidationTask/Program.cs
+++ ValidationTask/Program.cs
@@ -31,19 +31,36 @@
         static bool ValidName(string name)
         {
             // name must be at least two characters and contain only letters
+            if (name.Length < 2)
+            {
+                return false;
+            }
+            else if 
+            
+            return true;
         }
 
         static bool validAge(int age)
         {
             //age must be between 11 and 18 inclusive
-
+            if (age <= 18 && age >= 11)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
    
         static bool ValidPassword(string password)
         {
             // Check password is at least 8 characters in length
-
+            if (password.Length < 8)
+            {
+                return false;
+            }
 
             // Check password contains a mix of lower case, upper case and non letter characters
             // QWErty%^& = valid
@@ -56,8 +73,10 @@
             // AAbbdd!2 = valid (only 2 consecutive letters A and B and only 2 repeating of each)
             // abC461*+ = not valid (abC are 3 consecutive letters)
             // 987poiq! = not valid (987 are consecutive)
+            else if ()
+            {
 
-
+            }
 
         }
         static bool validEmail(string email)
@@ -68,6 +87,11 @@
             // has at least 2 characters after the .
             // contains only one @ and any number of .
             // does not contain any other non letter or number characters
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                return false;
+            }
+            int IndexOf = email.IndexOf("@");
 
         }
         static string createUserName(string firstName, string lastName, int age)
@@ -77,9 +101,8 @@
             // last two characters of last name
             // age
             //e.g. Bob Smith aged 34 would have the username Both34
-
-
-
+            string userName = firstName.Substring(0, 2) + lastName.Substring(0, 2) + age;   
+            return userName;
         }
 
     }
